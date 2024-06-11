c2e0c50f2873077b8c9b2342999c3a1ccd9b17ef
ChimeCommunity <chimecommunity@proton.me>
2024-06-11 18:09:27 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-curl -o https://raw.githubusercontent.com/ChimeCommunity/ChimeCommunityTool/main/test_file.txt
+tools\\curl.exe -JLO -k https://raw.githubusercontent.com/ChimeCommunity/ChimeCommunityTool/main/test_file.txt
